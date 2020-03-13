f47c0f5b36a12d73fec01dff73f95dd338f37a8a
Kimiko Suzuki <sksuzuki@email.unc.edu>
2020-03-13 18:16:40 -0400

    testing pip edirect

--- src/data_query.py
+++ src/data_query.py
@@ -2,14 +2,17 @@
 
 import os
 # import sys
-def download_data(accession_num):
-    os.system('curl -o ./protein_data_ncbi/{}.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/146/045/GCA_000146045.2_R64/GCA_000146045.2_R64_protein.faa.gz'.format(accession_num))
+def download_data(accession):
+    os.system('curl -o ./protein_data_ncbi/{}.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/146/045/GCA_000146045.2_R64/GCA_000146045.2_R64_protein.faa.gz'.format(accession))
 
 def get_protein_data(protein):
     os.system('bash ./src/filter_proteins.sh {}'.format(protein))
 
-
-def download_multiple_datasets():
+def download_multiple_datasets(accession_nums, proteins):
+    for accs in accession_nums:
+        accs = parse_accessions(accs)
+        # download_data()
+        print(accs)
     return
 
 def get_idxs(data, protein):
--- src/filter_proteins.sh
+++ src/filter_proteins.sh
@@ -4,4 +4,4 @@ FILES=./protein_data_ncbi/*.faa.gz
 for f in $FILES;
 do
         zcat $f | seqkit grep -i -r -n -p $1[p]*;
-done > $1_out.faa
+done > ./result_data/$1_out.faa
