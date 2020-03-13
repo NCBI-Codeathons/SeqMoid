ae9dba592a0dc63d809e8d297df58fb19dc62846
Kimiko Suzuki <sksuzuki@email.unc.edu>
2020-03-13 17:49:39 -0400

    testing directories

--- src/data_query.py
+++ src/data_query.py
@@ -2,13 +2,16 @@
 
 import os
 # import sys
+def download_data(accession_num):
+    os.system('curl -o ./protein_data_ncbi/{}.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/146/045/GCA_000146045.2_R64/GCA_000146045.2_R64_protein.faa.gz'.format(accession_num))
+
+def get_protein_data(protein):
+    os.system('bash ./src/filter_proteins.sh {}'.format(protein))
+
 
 def download_multiple_datasets():
     return
 
-def download_data(accession_num):
-    os.system('curl -o ./protein_data_ncbi/{}.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/146/045/GCA_000146045.2_R64/GCA_000146045.2_R64_protein.faa.gz'.format(accession_num))
-#
 def get_idxs(data, protein):
     return [idx for idx, j in enumerate(data.get_headers()) if protein.lower() in j.lower()]
 
@@ -16,12 +19,3 @@ def get_faa_data(idxs, data):
     gseqs = [data.get_seqs()[idx] for idx in idxs]
     gheaders = [data.get_headers()[idx] for idx in idxs]
     return gseqs, gheaders
-
-def get_protein_data(protein):
-    os.system('bash ./src/filter_proteins.sh {}'.format(protein))
-    # print(protein)
-
-def download_data(accession_num):
-    os.system('curl -o ./protein_data_ncbi/{}.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/146/045/GCA_000146045.2_R64/GCA_000146045.2_R64_protein.faa.gz'.format(accession_num))
-
-# !zcat ../protein_ncbi_data/GCA_000146045.2_R64_protein.faa.gz | seqkit grep -i -r -n -p Ste50[p]*
