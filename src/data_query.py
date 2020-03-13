98eec8e415aa432f729c6ee7a2a3e12ff704b9b1
Kimiko Suzuki <sksuzuki@email.unc.edu>
2020-03-13 14:30:09 -0400

    filter proteins

--- src/data_query.py
+++ src/data_query.py
@@ -3,7 +3,11 @@
 import os
 import sys
 
+def download_data(accession_num):
+    os.system('curl -o ../protein_data_ncbi/{}.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/146/045/GCA_000146045.2_R64/GCA_000146045.2_R64_protein.faa.gz'.format(accession_num))
 
+def unzip_data(accesssion_num, protein):
+    os.system('!zcat ../protein_ncbi_data/GCA_000146045.2_R64_protein.faa.gz | seqkit grep -i -r -n -p {}[p]*'.format{protein})
 
 def get_idxs(data, protein):
     return [idx for idx, j in enumerate(data.get_headers()) if protein.lower() in j.lower()]
@@ -12,3 +16,6 @@ def get_faa_data(idxs, data):
     gseqs = [data.get_seqs()[idx] for idx in idxs]
     gheaders = [data.get_headers()[idx] for idx in idxs]
     return gseqs, gheaders
+
+
+# !zcat ../protein_ncbi_data/GCA_000146045.2_R64_protein.faa.gz | seqkit grep -i -r -n -p Ste50[p]*
---
+++ src/filter_proteins.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env
+
+FILES=../protein_ncbi_data/*.faa.gz
+for f in $FILES;
+do
+        zcat $f | seqkit grep -i -r -n -p $1[p]*
+done > Ste50_out.faa
